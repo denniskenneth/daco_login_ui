88048063ef9d775e7e078e22e16829a04669f80a
Dennis Kenneth <dennyify123@gmail.com>
2022-02-18 23:00:36 +0000

    signin ui fixed

--- lib/features/daco/presentation/pages/sign_in.dart
+++ lib/features/daco/presentation/pages/sign_in.dart
@@ -10,109 +10,104 @@ class SigninPage extends StatelessWidget {
     var kHeight = size.height;
     var kWidth = size.width;
 
-    return Scaffold(
-        body: SafeArea(
-      child: Container(
-        width: kWidth,
-        height: kHeight,
-        child: GridView.count(
-          // physics: const BouncingScrollPhysics(),
-          // primary: false,
-          childAspectRatio: 2/0.8,
-          crossAxisCount: 1,
-          padding: EdgeInsets.only(left: 24.0, right: 24.0),
-          mainAxisSpacing: 90,
-          // crossAxisSpacing: 1000,
-          // shrinkWrap: true,
-          children: [
-            IconButton(
-              padding: EdgeInsets.zero,
-              autofocus: true,
-              onPressed: () => Navigator.pop(context),
-              alignment: Alignment.centerLeft,
-              icon: const Icon(Icons.arrow_back),
-
-            ),
-            Container(
-              // margin: EdgeInsets.only(top: 15),
-              child: RichText(
-                text: TextSpan(children: [
-                  TextSpan(
-                    text: 'Let\'s sign you in.\n\n',
-                    style: kDefaultTextTheme().headline4,
-                  ),
-                  TextSpan(
-                    text: 'Welcome back.\nYou\'ve been missed!',
-                    style: kDefaultTextTheme().headline6,
-                  ),
-                  // TextSpan(text: 'You\'ve been missed.')
-                ]),
+    return SafeArea(
+        child: Scaffold(
+            // backgroundColor: useDarkTheme(con).scaffoldBackgroundColor,
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: useDarkTheme(context).scaffoldBackgroundColor,
+              leading: GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: Icon(Icons.arrow_back),
               ),
             ),
-            // SizedBox(
-            //   height: 50,
-            // ),
-            Column(
-              children: [
-                const TextField(
-                  scrollPadding: EdgeInsets.only(bottom: 10),
-                  decoration: InputDecoration(
-                    labelText: 'Phone, email or username',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(15),
-                            bottomLeft: Radius.circular(15),
-                            topRight: Radius.circular(15),
-                            bottomRight: Radius.circular(15))),
+            body: Container(
+                width: kWidth,
+                height: kHeight,
+                padding: EdgeInsets.only(left: 24.0,right: 24.0),
+                child: SingleChildScrollView(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: kHeight * 0.8,
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          // margin: EdgeInsets.only(top: 15),
+                          child: RichText(
+                            text: TextSpan(children: [
+                              TextSpan(
+                                text: 'Let\'s sign you in.\n\n',
+                                style: kDefaultTextTheme().headline4,
+                              ),
+                              TextSpan(
+                                text: 'Welcome back.\nYou\'ve been missed!',
+                                style: kDefaultTextTheme().headline6,
+                              ),
+                              // TextSpan(text: 'You\'ve been missed.')
+                            ]),
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            const TextField(
+                              scrollPadding: EdgeInsets.only(bottom: 10),
+                              decoration: InputDecoration(
+                                labelText: 'Phone, email or username',
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(15),
+                                        bottomLeft: Radius.circular(15),
+                                        topRight: Radius.circular(15),
+                                        bottomRight: Radius.circular(15))),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 10),
+                              child: TextField(
+                                scrollPadding: EdgeInsets.only(bottom: 30),
+                                decoration: InputDecoration(
+                                    labelText: 'Password',
+                                    border: const OutlineInputBorder(
+                                        borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(15),
+                                            bottomLeft: Radius.circular(15),
+                                            topRight: Radius.circular(15),
+                                            bottomRight: Radius.circular(15))),
+                                    suffixIcon: IconButton(
+                                      icon: const Icon(Icons.wifi_password_outlined),
+                                      onPressed: () {},
+                                    )),
+                              ),
+                            )
+                          ],
+                        ),
+                        Column(
+                          // mainAxisSize: MainAxisSize.max,
+                          // mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            RichText(
+                              text: const TextSpan(children: [
+                                TextSpan(text: 'Don\'t have an account? '),
+                                TextSpan(text: 'Register')
+                              ]),
+                            ),
+                            SizedBox(
+                                width: double.infinity,
+                                child: TextButton(
+                                  onPressed: () {},
+                                  child: const Text('Sign In'),
+                                  style: TextButton.styleFrom(
+                                      backgroundColor: const Color(0xffE4E8EE),
+                                      primary: const Color(0xff050406)),
+                                ))
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 10),
-                  child: TextField(
-                    scrollPadding: EdgeInsets.only(bottom: 30),
-                    decoration: InputDecoration(
-                        labelText: 'Password',
-                        border: const OutlineInputBorder(
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(15),
-                                bottomLeft: Radius.circular(15),
-                                topRight: Radius.circular(15),
-                                bottomRight: Radius.circular(15))),
-                        suffixIcon: IconButton(
-                          icon: const Icon(Icons.wifi_password_outlined),
-                          onPressed: () {},
-                        )),
-                  ),
-                )
-              ],
-            ),
-            // SizedBox(
-            //   height: 50,
-            // ),
-            Column(
-              // mainAxisSize: MainAxisSize.max,
-              // mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                RichText(
-                  text: const TextSpan(children: [
-                    TextSpan(text: 'Don\'t have an account? '),
-                    TextSpan(text: 'Register')
-                  ]),
-                ),
-                SizedBox(
-                    width: double.infinity,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text('Sign In'),
-                      style: TextButton.styleFrom(
-                          backgroundColor: const Color(0xffE4E8EE),
-                          primary: const Color(0xff050406)),
-                    ))
-              ],
-            )
-          ],
-        ),
-      ),
-    ));
+                ))));
   }
 }
