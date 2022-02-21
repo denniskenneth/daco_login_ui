219013ec354acf9d81c167763264d93278ad7657
Dennis Kenneth <dennyify123@gmail.com>
2022-02-21 16:06:42 +0000

    landscape responsiveness added

--- lib/features/daco/presentation/pages/home.dart
+++ lib/features/daco/presentation/pages/home.dart
@@ -13,96 +13,205 @@ class HomePage extends StatelessWidget {
     var kHeight = size.height;
     var kWidth = size.width;
     var kTextTheme = kDefaultTextTheme();
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
 
     final String assetName = 'assets/static/collaboration-team-svgrepo-com.svg';
 
     return Scaffold(
-      body: Container(
-        width: kWidth,
-        height: kHeight,
-        padding: EdgeInsets.only(
-            top: 20, bottom: kHeight * 0.02, right: 10, left: 10),
-        decoration: const BoxDecoration(
-            // color: Colors.white,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(right: 10, left: 10),
+          decoration: const BoxDecoration(
+              // color: Colors.white,
 
-            ),
-        child: SafeArea(
-          child: Column(
-            children: [
-              SvgPicture.asset(
-                  assetName,
-                  color: null,
-                  semanticsLabel: 'A red up arrow',
-                height: kHeight * 0.5,
               ),
-              Column(
-                children:  [
-                  RichText(
-                    textAlign: TextAlign.center,
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                              text: 'Enterprise team\ncollaboration.',
-                              style: kTextTheme.headline4,
-                          ),
-
-                        ]
-                      )
-                  ),
-
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: RichText(
-                  textAlign: TextAlign.center,
-                  text:  TextSpan(
-                    style: kTextTheme.bodyText1,
+          child: SingleChildScrollView(child: (() {
+            if (!isLandscape) {
+              return SizedBox(
+                  height: kHeight * 0.9,
+                  width: kWidth,
+                  child: Column(
                     children: [
-                      TextSpan(
-                        text: 'Bring together your files, your tools,\nprojects and people. Including a new\nmobile and desktop application',
-                        style: kTextTheme.bodyText1?.copyWith(
-                          letterSpacing: 0.1,
-                          height: 1.5
-                        )
-                      )
-                    ]
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.center,
+                      SvgPicture.asset(
+                        assetName,
+                        color: null,
+                        semanticsLabel: 'A red up arrow',
+                        height: kHeight * 0.5,
+                      ),
+                      Column(
+                        children: [
+                          RichText(
+                              textAlign: TextAlign.center,
+                              text: TextSpan(children: [
+                                TextSpan(
+                                  text: 'Enterprise team\ncollaboration.',
+                                  style: kTextTheme.headline4,
+                                ),
+                              ])),
+                        ],
+                      ),
+                      RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(style: kTextTheme.bodyText1, children: [
+                          TextSpan(
+                              text:
+                                  'Bring together your files, your tools,\nprojects and people. Including a new\nmobile and desktop application',
+                              style: kTextTheme.bodyText1
+                                  ?.copyWith(letterSpacing: 0.1, height: 1.5))
+                        ]),
+                      ),
+                      Expanded(
+                        child: Stack(
+                          alignment: AlignmentDirectional.bottomStart,
+                          children: [
+                            Positioned(
+                              right: kWidth * 0.22,
+                              child: TextButton(
+                                  onPressed: () => Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const SigninPage())),
+                                  style: TextButton.styleFrom(
+                                    backgroundColor: Color(0x0ff3B3A42),
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 15, horizontal: 30),
+                                    textStyle: kTextTheme.bodyText1?.copyWith(
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 12,
+                                        letterSpacing: 0.2),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    'Sign In',
+                                    style: TextStyle(color: Color(0xffF4F4F5)),
+                                  )),
+                            ),
+                            Positioned(
+                              left: kWidth * 0.23,
+                              child: ElevatedButton(
+                                  onPressed: () => Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const RegisterPage())),
+                                  style: TextButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 15, horizontal: 30),
+                                    textStyle: kTextTheme.bodyText1?.copyWith(
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 12,
+                                        letterSpacing: 0.2),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    'Register',
+                                    style: TextStyle(color: Color(0xff29262E)),
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ));
+            } else {
+              return Container(
+                height: kHeight + (kHeight * 0.3),
+                width: kWidth,
+                child: Column(
                   children: [
-                    TextButton(
-                        onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const RegisterPage())),
-                        style: TextButton.styleFrom(
-                            backgroundColor: Colors.white,
-                            textStyle: const TextStyle(
-                              color: Colors.black,
-                            )),
-                        child: const Text(
-                          'Register',
-                          style: TextStyle(color: Color(0xff29262E)),
-                        )),
-                    TextButton(
-                        onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context)=> const SigninPage())),
-                        style: TextButton.styleFrom(
-                            backgroundColor: Color(0x0ff3B3A42),
-                            textStyle: const TextStyle()),
-                        child: const Text(
-                          'Sign In',
-                          style: TextStyle(color: Color(0xffF4F4F5)),
-                        )),
-
-
+                    SvgPicture.asset(
+                      assetName,
+                      color: null,
+                      semanticsLabel: 'A red up arrow',
+                      height: kHeight * 0.5,
+                    ),
+                    Column(
+                      children: [
+                        RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(children: [
+                              TextSpan(
+                                text: 'Enterprise team\ncollaboration.',
+                                style: kTextTheme.headline4,
+                              ),
+                            ])),
+                      ],
+                    ),
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(style: kTextTheme.bodyText1, children: [
+                        TextSpan(
+                            text:
+                            'Bring together your files, your tools,\nprojects and people. Including a new\nmobile and desktop application',
+                            style: kTextTheme.bodyText1
+                                ?.copyWith(letterSpacing: 0.1, height: 1.5))
+                      ]),
+                    ),
+                    Expanded(
+                      child: Stack(
+                        alignment: AlignmentDirectional.bottomStart,
+                        children: [
+                          Positioned(
+                            right: kWidth * 0.3,
+                            child: TextButton(
+                                onPressed: () => Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                        const SigninPage())),
+                                style: TextButton.styleFrom(
+                                  backgroundColor: Color(0x0ff3B3A42),
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 15, horizontal: 40),
+                                  textStyle: kTextTheme.bodyText1?.copyWith(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 12,
+                                      letterSpacing: 0.2),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Sign In',
+                                  style: TextStyle(color: Color(0xffF4F4F5)),
+                                )),
+                          ),
+                          Positioned(
+                            left: kWidth * 0.3,
+                            child: ElevatedButton(
+                                onPressed: () => Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                        const RegisterPage())),
+                                style: TextButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 15, horizontal: 40),
+                                  textStyle: kTextTheme.bodyText1?.copyWith(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 12,
+                                      letterSpacing: 0.2),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Register',
+                                  style: TextStyle(color: Color(0xff29262E)),
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
-              ),
-
-            ],
-          ),
+              );
+            }
+          })()),
         ),
       ),
     );
--- lib/features/daco/presentation/pages/sign_in.dart
+++ lib/features/daco/presentation/pages/sign_in.dart
@@ -2,8 +2,18 @@ import 'package:daco_login/utils/theme.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class SigninPage extends StatelessWidget {
+class SigninPage extends StatefulWidget {
   const SigninPage({Key? key}) : super(key: key);
+
+  @override
+  State<SigninPage> createState() => _SigninPageState();
+}
+
+class _SigninPageState extends State<SigninPage> {
+
+  final _emailController = TextEditingController(),
+  _passwordController = TextEditingController()
+  ;
 
   @override
   Widget build(BuildContext context) {
@@ -22,102 +32,111 @@ class SigninPage extends StatelessWidget {
             child: Icon(Icons.arrow_back),
           ),
         ),
-        body: Container(
-            width: kWidth,
-            height: kHeight,
-            padding: EdgeInsets.only(left: 24.0,right: 24.0),
-            child: SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  minHeight: kHeight * 0.8,
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      // margin: EdgeInsets.only(top: 15),
-                      child: RichText(
-                        text: TextSpan(children: [
-                          TextSpan(
-                            text: 'Let\'s sign you in.\n\n',
-                            style: kDefaultTextTheme().headline4,
-                          ),
-                          TextSpan(
-                            text: 'Welcome back.\nYou\'ve been missed!',
-                            style: kDefaultTextTheme().headline6,
-                          ),
-                          // TextSpan(text: 'You\'ve been missed.')
-                        ]),
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        const TextField(
-                          scrollPadding: EdgeInsets.only(bottom: 10),
-                          decoration: InputDecoration(
-                            labelText: 'Phone, email or username',
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(15),
-                                    bottomLeft: Radius.circular(15),
-                                    topRight: Radius.circular(15),
-                                    bottomRight: Radius.circular(15))),
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 10),
-                          child: TextField(
-                            scrollPadding: EdgeInsets.only(bottom: 30),
-                            decoration: InputDecoration(
-                                labelText: 'Password',
-                                border: const OutlineInputBorder(
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(15),
-                                        bottomLeft: Radius.circular(15),
-                                        topRight: Radius.circular(15),
-                                        bottomRight: Radius.circular(15))),
-                                suffixIcon: IconButton(
-                                  icon: const Icon(Icons.wifi_password_outlined),
-                                  onPressed: () {},
-                                )),
-                          ),
-                        )
-                      ],
-                    ),
-                    Column(
-                      // mainAxisSize: MainAxisSize.max,
-                      // mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        RichText(
-                          text:  TextSpan(children: [
+        body: SafeArea(
+          child: Container(
+              width: kWidth,
+              height: kHeight,
+              padding: EdgeInsets.only(left: 24.0,right: 24.0),
+              child: SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: kHeight * 0.8,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        // margin: EdgeInsets.only(top: 15),
+                        child: RichText(
+                          text: TextSpan(children: [
                             TextSpan(
-                              text: 'Don\'t have an account? ',
-                              style: kTextTheme.subtitle2?.copyWith(
-                                color: Color(0xffe4e8ee).withOpacity(0.4),
-                              )
+                              text: 'Let\'s sign you in.\n\n',
+                              style: kDefaultTextTheme().headline4,
                             ),
                             TextSpan(
-                                text: 'Register',
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () => print('Register'),
-                            )
+                              text: 'Welcome back.\nYou\'ve been missed!',
+                              style: kDefaultTextTheme().headline6,
+                            ),
+                            // TextSpan(text: 'You\'ve been missed.')
                           ]),
                         ),
-                        SizedBox(
-                            width: double.infinity,
-                            child: TextButton(
-                              onPressed: () {},
-                              child: const Text('Sign In'),
-                              style: TextButton.styleFrom(
-                                  backgroundColor: const Color(0xffE4E8EE),
-                                  primary: const Color(0xff050406)),
-                            ))
-                      ],
-                    )
-                  ],
+                      ),
+                      Column(
+                        children: [
+                           TextField(
+                            controller: _emailController,
+                            scrollPadding: EdgeInsets.only(bottom: 10),
+                            decoration: const InputDecoration(
+                              labelText: 'Phone, email or username',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(15),
+                                      bottomLeft: Radius.circular(15),
+                                      topRight: Radius.circular(15),
+                                      bottomRight: Radius.circular(15))),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 10),
+                            child: TextField(
+                              controller: _passwordController,
+                              scrollPadding: EdgeInsets.only(bottom: 30),
+                              decoration: InputDecoration(
+                                  labelText: 'Password',
+                                  border: const OutlineInputBorder(
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(15),
+                                          bottomLeft: Radius.circular(15),
+                                          topRight: Radius.circular(15),
+                                          bottomRight: Radius.circular(15))),
+                                  suffixIcon: IconButton(
+                                    icon: const Icon(Icons.wifi_password_outlined),
+                                    onPressed: () {},
+                                  )),
+                            ),
+                          )
+                        ],
+                      ),
+                      Column(
+                        // mainAxisSize: MainAxisSize.max,
+                        // mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          RichText(
+                            text:  TextSpan(children: [
+                              TextSpan(
+                                text: 'Don\'t have an account? ',
+                                style: kTextTheme.subtitle2?.copyWith(
+                                  color: Color(0xffe4e8ee).withOpacity(0.4),
+                                )
+                              ),
+                              TextSpan(
+                                  text: 'Register',
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () => print('Register'),
+                              )
+                            ]),
+                          ),
+                          SizedBox(
+                              width: double.infinity,
+                              child: TextButton(
+                                onPressed: () {},
+                                child: const Text('Sign In'),
+                                style: TextButton.styleFrom(
+                                    backgroundColor: const Color(0xffE4E8EE),
+                                    primary: const Color(0xff050406)),
+                              ))
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )));
+              )),
+        ));
+  }
+
+  void _validAndSignin() {
+    var email = _emailController.text.trim();
+    var password = _passwordController.text.trim();
   }
 }
